625f547d29e5e72efa2a86f043f1a26a233d4326
M.Faisal Amin <faisalamin9696@gmail.com>
2022-06-06 13:03:59 +0500

    initial push lecture 1,2,3,4,5

---
+++ app/src/main/java/com/example/hivelearners/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.hivelearners;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
