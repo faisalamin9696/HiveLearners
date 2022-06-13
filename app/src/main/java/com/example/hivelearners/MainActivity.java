1ebb32b5a4340b53afa04351015ea48acd83137d
M.Faisal Amin <faisalamin9696@gmail.com>
2022-06-13 17:21:33 +0500

    Lecture 9 updated

--- app/src/main/java/com/example/hivelearners/MainActivity.java
+++ app/src/main/java/com/example/hivelearners/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.hivelearners;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button start_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        start_btn =  findViewById(R.id.start_btn);
+
+        start_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
+
     }
 }
