51294a400c74a0194d21632d370f88e5db265d9b
M.Faisal Amin <faisalamin9696@gmail.com>
2022-06-17 17:28:05 +0500

    Lecture 12 updated

--- app/src/main/java/com/example/hivelearners/MainActivity.java
+++ app/src/main/java/com/example/hivelearners/MainActivity.java
@@ -1,29 +1,43 @@
 package com.example.hivelearners;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button start_btn;
+    Button signin_btn;
+    EditText email_et, pass_et;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        start_btn =  findViewById(R.id.start_btn);
+        signin_btn = findViewById(R.id.signin_btn);
+        email_et = findViewById(R.id.email_et);
+        pass_et = findViewById(R.id.pass_et);
 
-        start_btn.setOnClickListener(new View.OnClickListener() {
+
+        signin_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                String email = email_et.getText().toString();
+                String pass = pass_et.getText().toString();
+
+                String result = email + ' ' + pass;
+
+                Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT).show();
+
+
             }
         });
-
-
 
 
     }
