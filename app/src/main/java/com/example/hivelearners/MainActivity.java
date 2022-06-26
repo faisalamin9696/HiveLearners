f86d5822c98cc9373f3d416a744c0e918be6f006
faisa <faisalamin9696@gmail.com>
2022-06-26 17:14:26 +0500

    Lecture 16 Update

--- app/src/main/java/com/example/hivelearners/MainActivity.java
+++ app/src/main/java/com/example/hivelearners/MainActivity.java
@@ -11,8 +11,10 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView screen1_tv;
-    Button num1_btn, num2_btn, num3_btn, num4_btn, num5_btn, num6_btn, num7_btn, num8_btn, num9_btn, num0_btn, decimal_btn;
+    TextView screen1_tv, screen2_tv;
+    Button num1_btn, num2_btn, num3_btn, num4_btn, num5_btn, num6_btn, num7_btn,
+            num8_btn, num9_btn, num0_btn, decimal_btn, plus_btn, minus_btn, equal_btn;
+    int first_num;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         screen1_tv = findViewById(R.id.screen1_tv);
+        screen2_tv = findViewById(R.id.screen2_tv);
+
 
         num1_btn = findViewById(R.id.num1_btn);
         num2_btn = findViewById(R.id.num2_btn);
@@ -32,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
         num9_btn = findViewById(R.id.num9_btn);
         num0_btn = findViewById(R.id.num0_btn);
         decimal_btn = findViewById(R.id.decimal_btn);
+        plus_btn = findViewById(R.id.plus_btn);
+        minus_btn = findViewById(R.id.minus_btn);
+        equal_btn = findViewById(R.id.equal_btn);
 
 
         num1_btn.setOnClickListener(new View.OnClickListener() {
@@ -131,6 +138,44 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        plus_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                screen2_tv.setText(plus_btn.getText().toString());
+                first_num = Integer.parseInt(screen1_tv.getText().toString());
+                screen1_tv.setText("");
+
+            }
+        });
+
+        minus_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                screen2_tv.setText(minus_btn.getText().toString());
+                first_num = Integer.parseInt(screen1_tv.getText().toString());
+                screen1_tv.setText("");
+
+            }
+        });
+
+        equal_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String stored_sign = screen2_tv.getText().toString();
+                if (stored_sign.equals("+")) {
+                    int result = first_num + Integer.parseInt(screen1_tv.getText().toString());
+                    Toast.makeText(MainActivity.this, String.valueOf(result), Toast.LENGTH_SHORT).show();
+
+                }
+                if (stored_sign.equals("-")) {
+                    int result = first_num - Integer.parseInt(screen1_tv.getText().toString());
+                    Toast.makeText(MainActivity.this, String.valueOf(result), Toast.LENGTH_SHORT).show();
+
+                }
+
+
+            }
+        });
 
     }
 }
